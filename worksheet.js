d6c9fe7c1579a21e42f032470f10b6c69116f730
Jalen Arrington <jmarrington178@yahoo.com>
2022-09-02 17:10:15 -0400

    Added some structure to make it more clear what was being worked on

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,14 @@
-//  Task 1
+// Variables
+// Task 1 - unfinished
 // let dayOfWeek = 'Friday';
 // console.log(dayOfWeek);
 // let dayOfWeek = 'Friday';
 
 // Task 2
-animalInput = prompt("What is your favorite animal?")
+// animalInput = prompt("What is your favorite animal?")
+// colorInput = prompt("What is your favorite color?")
+// bothChoices = console.log(`I've never seen a ${colorInput} ${animalInput}`)
+// console.log(bothChoices)
 
-colorInput = prompt("What is your favorite color?")
 
-bothChoices = console.log(`I've never seen a ${colorInput} ${animalInput}`)
-console.log(bothChoices)
+Task 3
